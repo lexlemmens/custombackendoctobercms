be37ebabe1f1caf18fbf9f8ced4dc262edb3318c
Lex Lemmens <lex@notonpaper.nl>
2017-02-13 16:56:55 +0100

    First commit

---
+++ custombackend/Plugin.php
@@ -0,0 +1,57 @@
+<?php namespace lexlemmens\CustomBackend;
+
+use System\Classes\PluginBase;
+use Illuminate\Support\Facades\Event;
+use Backend\Facades\Backend;
+use Backend\Classes\Controller as BackendController;
+use System\Models\PluginVersion;
+
+/**
+ * Class Plugin
+ * @package lexlemmens\CustomBackend
+ */
+class Plugin extends PluginBase
+{
+    /**
+     * Run this code at boot.
+     */
+    public function boot() {
+
+        Event::listen('backend.menu.extendItems', function($manager) {
+
+            $manager->addMainMenuItems('October.Backend', [
+                'dashboard' => [
+                    'icon'        => 'icon-dashboard'
+                ],
+            ]);
+
+            $manager->addMainMenuItems('October.System', [
+                'system' => [
+                    'icon' => 'icon-cogs'
+                ],
+            ]);
+
+            $manager->addMainMenuItems('October.Cms', [
+                'media' => [
+                    'icon' => 'icon-camera'
+                ],
+                'cms' => [
+                    'icon' => 'icon-edit'
+                ],
+            ]);
+
+            if(PluginVersion::where('code', 'RainLab.Pages')->where('is_disabled', 0)->first()){
+                $manager->addMainMenuItems('RainLab.Pages', [
+                    'pages' => [
+                        'icon' => 'icon-file'
+                    ]
+                ]);
+            }
+
+        });
+
+        BackendController::extend(function ($controller) {
+            $controller->addCss('/plugins/lexlemmens/custombackend/assets/css/custom.css');
+        });
+    }
+}
---
+++ custombackend/lang/en/lang.php
@@ -0,0 +1,6 @@
+<?php return [
+    'plugin' => [
+        'name' => 'Custom Backend',
+        'description' => 'Edit your backend icons and css with this plug-in'
+    ]
+];
